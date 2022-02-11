9961be44fcd9776648154838e86a1822d68d87cd
przygoda <schmidt.multimedia@googlemail.com>
2022-02-11 08:31:15 +0000

    02 Tables / Tabelle anlegen

--- 02_SQL_Tables/02_db_table_SQL.sql
+++ 02_SQL_Tables/02_db_table_SQL.sql
@@ -3,3 +3,24 @@
 /* Datenbanken auf Server anzeigen */
 SHOW DATABASES;
 
+/* DB boo löschen, falls vorhanden*/
+DROP DATABASE IF EXISTS boo;
+
+/* DB boo anlegen, falls noch nicht vorhanden*/
+CREATE DATABASE IF NOT EXISTS boo;
+
+/* DB auswählen */
+USE boo;
+
+/* Tabelle anlegen, falls noch nicht vorhanden */
+CREATE TABLE IF NOT EXISTS test
+(
+	name VARCHAR(20),
+	age INT
+);
+
+/* Alle Tabellen in der DB anzeigen */
+SHOW TABLES;
+
+/* Struktur der Tabelle anzeigen */
+DESCRIBE test;
