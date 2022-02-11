fa7b50b4d038a6409412de6bcfb8623abfa05856
przygoda <schmidt.multimedia@googlemail.com>
2022-02-11 10:16:42 +0000

    02a /  NOT NULL  / DEFAULT

--- 02_SQL_Tables/02a_db_table_SQL.sql
+++ 02_SQL_Tables/02a_db_table_SQL.sql
@@ -17,8 +17,8 @@ CREATE DATABASE IF NOT EXISTS boo;
 /* Tabelle anlegen, falls noch nicht vorhanden */
 CREATE TABLE IF NOT EXISTS boo.test
 (
-	name VARCHAR(20),
-	age INT
+	name VARCHAR(20) NOT NULL DEFAULT "TBA",
+	age INT NOT NULL DEFAULT 0
 );
 
 /* Alle Tabellen in der DB anzeigen */
@@ -31,6 +31,9 @@ DESCRIBE boo.test;
 INSERT INTO boo.test(name,age) VALUES ("Grizabella",29);
 INSERT INTO boo.test(age,name) VALUES (35,"Alonzo");
 INSERT INTO boo.test VALUES ();
+
+-- Doppelte Datensätze werden zugelassen !
+INSERT INTO boo.test(age,name) VALUES (35,"Alonzo");
 
 /* ---- Inhalte der Tabelle anzeigen ---- */
 SELECT * FROM boo.test;
