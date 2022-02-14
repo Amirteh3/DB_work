5453ec15c9a459c56a97e6c8875855c27d04b810
przygoda <schmidt.multimedia@googlemail.com>
2022-02-14 08:12:22 +0000

    03 / users / Struktur

--- 03_user_DB/03_db_userDB_SQL.sql
+++ 03_user_DB/03_db_userDB_SQL.sql
@@ -1,2 +1,23 @@
+/* USER DB Vers. 1 */
 
-SHOW DATABASES;
+/* DB */
+DROP DATABASE IF EXISTS boo;
+CREATE DATABASE IF NOT EXISTS boo;
+
+/* Table users */
+DROP TABLE IF EXISTS boo.users;
+
+CREATE TABLE IF NOT EXISTS boo.users
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    user_name VARCHAR(20) NOT NULL UNIQUE,
+    family_name VARCHAR(20) NOT NULL,
+    first_name VARCHAR(20) NOT NULL
+);
+
+DESCRIBE boo.users;
+
+
+
+
+
