55c520e377c02e8026639c307c0b789600376abb
przygoda <schmidt.multimedia@googlemail.com>
2022-02-14 11:59:02 +0000

    04b /  UPDATE

---
+++ 04_SQL_DataImport/04b_db_csv_import.sql
@@ -0,0 +1,12 @@
+/* CRUD Create | Read | Update | Delete */  
+
+-- CREATE: siehe 04_ ...ALTER
+
+SELECT * FROM boo.cats;
+
+/* UPDATE --> Änderung(en) in bestehender Tab. */
+UPDATE boo.cats SET cat_name = "Alonzo" WHERE id = 2;
+UPDATE boo.cats SET fur_color = "grey" WHERE id = 3;
+
+
+SELECT * FROM boo.cats;
